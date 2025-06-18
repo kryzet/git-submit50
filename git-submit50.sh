962e86bc1921ec02c40fcabe83339c38337ec7dd
kryzet <166802607+kryzet@users.noreply.github.com>
2025-06-18 21:24:02 +0400

    implement some logic

--- git-submit50.sh
+++ git-submit50.sh
@@ -5,3 +5,12 @@
 # A reference to an unset variable (oddly enough) or a failure upon executing a
 # command does not cause the shell to terminate script interpretation.
 set -eu
+
+branch="$1"
+assignment_dir=$PWD
+ref=$(git symbolic-ref --short HEAD)
+if git rev-parse --verify --end-of-options $branch^{branch}; then
+    git switch $branch
+else
+    git checkout --orphan $branch
+fi
