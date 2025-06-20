c34b2fdfcc76c1f7b3b016b796a59cc60eb7a144
kryzet <166802607+kryzet@users.noreply.github.com>
2025-06-20 23:21:04 +0400

    better comments

--- git-submit50.sh
+++ git-submit50.sh
@@ -1,24 +1,25 @@
-# When bash is invoked like this, it will provide a POSIX-conformant
-# environment. Suggestions for making the script more portable are welcome.
+# When bash is invoked like this, it will behave as a POSIX-conformant
+# command interpreter. However, this can only be expected to work on numerous
+# Linux distributions and not on OS X (#1, #5) or other POSIX or SUS platforms
+# (#2, #4).
 #!/usr/bin/env sh
 
-# A failure upon executing a command or (oddly enough) a reference to an unset
-# variable does not cause the shell to terminate script interpretation.
+# Oddly enough, a reference to an unset variable does not cause the shell
+# to exit. See the POSIX manpage for `set` or
+# https://pubs.opengroup.org/onlinepubs/9699919799.2018edition/utilities/V3_chap02.html#set
+# to learn more.
 set -eu
 
-# First argument passed to the script is the CS50 slug, which is in fact a git
-# branch
+# See Usage in the README. #6
 branch="$1"
 
 # submit50 expects to be invoked from within the directory where the files to
 # be submitted are, so it is reasonable to simply make the same assumption.
-# This may be a supporting detail for an argument to write this utility in a
-# language that's not shell, so that files can be found in a more flexible
-# way :)
+# This is totally a valid area of improvement, though.
 assignment_dir=$PWD
 
-# Determine the current branch. This will be the ref used to update the
-# submission contents to the latest snapshot.
+# This will be the ref used to update the submission contents to the latest
+# snapshot.
 ref=$(git symbolic-ref --short HEAD)
 
 # Switch to the slug branch
